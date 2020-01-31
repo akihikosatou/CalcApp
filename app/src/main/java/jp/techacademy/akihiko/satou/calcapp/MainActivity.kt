c613d648373c72254452e8fd3ed22e5599e35ab5
akihiko.satou <ak.satou@scsk.jp>
2020-01-31 17:08:32 +0900

    Lesson4-5_01

--- app/src/main/java/jp/techacademy/akihiko/satou/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/akihiko/satou/calcapp/MainActivity.kt
@@ -1,12 +1,57 @@
 package jp.techacademy.akihiko.satou.calcapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        var num:Float= 0F
+        var i1:Float = 0F
+        var i2:Float = 0F
+        val intent = Intent(this, SecondActivity::class.java)
+
+        if( editText1.text.toString().length == 0){
+            i1 = 0F
+        }else {
+            i1 = editText1.text.toString().toFloat()
+        }
+        if( editText2.text.toString().length == 0){
+            i2 = 0F
+        }else {
+            i2 = editText2.text.toString().toFloat()
+        }
+
+        if (v.id == R.id.button1) {
+            num = i1 + i2
+            intent.putExtra("VALUE1", num)
+        } else if (v.id == R.id.button2) {
+            num = i1 - i2
+            intent.putExtra("VALUE1", num)
+        } else if (v.id == R.id.button3) {
+            num = i1 * i2
+            intent.putExtra("VALUE1", num)
+        } else if (v.id == R.id.button4) {
+            num = i1 / i2
+            intent.putExtra("VALUE1", num)
+        }
+        startActivity(intent)
     }
 }
--- app/src/main/java/jp/techacademy/akihiko/satou/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/akihiko/satou/calcapp/SecondActivity.kt
@@ -2,11 +2,17 @@ package jp.techacademy.akihiko.satou.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val value1 = intent.getFloatExtra("VALUE1", 0F)
+        val value2 = intent.getFloatExtra("VALUE2", 0F)
+
+        textView.text = "${value1 + value2}"
     }
 }
